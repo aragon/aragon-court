51e717dc836e2e708a7e477eb41982bcaf0a15fe
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2019-10-02 10:30:12 +0900

    accounting: write inline documentation

--- contracts/court/CourtAccounting.sol
+++ contracts/court/CourtAccounting.sol
@@ -20,22 +20,36 @@ contract CourtAccounting is Controlled, ERC20Recoverable, IAccounting {
     string private constant ERROR_WITHDRAW_AMOUNT_ZERO = "ACCOUNTING_WITHDRAW_AMOUNT_ZERO";
     string private constant ERROR_WITHDRAW_INVALID_AMOUNT = "ACCOUNTING_WITHDRAW_INVALID_AMOUNT";
 
+    // List of balances indexed by token and holder address
     mapping (address => mapping (address => uint256)) internal balances;
 
     event Assign(ERC20 indexed token, address indexed from, address indexed to, uint256 amount);
     event Withdraw(ERC20 indexed token, address indexed from, address indexed to, uint256 amount);
 
+    /**
+    * @dev Ensure the msg.sender is the accounting's owner
+    */
     modifier onlyOwner {
         address owner = _accountingOwner();
         require(msg.sender == owner, ERROR_SENDER_NOT_OWNER);
         _;
     }
 
+    /**
+    * @dev Constructor function
+    * @param _controller Address of the controller
+    */
     constructor(Controller _controller) ERC20Recoverable(_controller) public {
         // solium-disable-previous-line no-empty-blocks
         // No need to explicitly call `Controlled` constructor since `ERC20Recoverable` is already doing it
     }
 
+    /**
+    * @notice Assign `@tokenAmount(_token, _amount)` to `_to`
+    * @param _token ERC20 token to be assigned
+    * @param _to Address of the recipient that will be assigned the tokens to
+    * @param _amount Amount of tokens to be assigned to the recipient
+    */
     function assign(ERC20 _token, address _to, uint256 _amount) external onlyOwner {
         require(_amount > 0, ERROR_DEPOSIT_AMOUNT_ZERO);
 
@@ -44,6 +58,12 @@ contract CourtAccounting is Controlled, ERC20Recoverable, IAccounting {
         emit Assign(_token, msg.sender, _to, _amount);
     }
 
+    /**
+    * @notice Withdraw `@tokenAmount(_token, _amount)` from sender to `_to`
+    * @param _token ERC20 token to be withdrawn
+    * @param _to Address of the recipient that will receive the tokens
+    * @param _amount Amount of tokens to be withdrawn from the sender
+    */
     function withdraw(ERC20 _token, address _to, uint256 _amount) external {
         uint256 balance = balanceOf(_token, msg.sender);
         require(_amount > 0, ERROR_WITHDRAW_AMOUNT_ZERO);
@@ -56,6 +76,12 @@ contract CourtAccounting is Controlled, ERC20Recoverable, IAccounting {
         require(_token.safeTransfer(_to, _amount), ERROR_WITHDRAW_FAILED);
     }
 
+    /**
+    * @dev Tell the token balance of a certain holder
+    * @param _token ERC20 token balance being queried
+    * @param _holder Address of the holder querying the balance of
+    * @return Amount of tokens the holder owns
+    */
     function balanceOf(ERC20 _token, address _holder) public view returns (uint256) {
         return balances[address(_token)][_holder];
     }

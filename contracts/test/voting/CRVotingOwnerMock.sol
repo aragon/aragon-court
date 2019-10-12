1a032ee77c5e4d4723e981cc3eeb6ed95c741915
ßingen <bingen@aragon.one>
2019-10-12 14:51:50 +0200

    Court & Voting: compute weight only on commit
    Adapt tests.

--- contracts/test/voting/CRVotingOwnerMock.sol
+++ contracts/test/voting/CRVotingOwnerMock.sol
@@ -5,6 +5,10 @@ import "../../voting/ICRVotingOwner.sol";
 
 
 contract CRVotingOwnerMock is ICRVotingOwner {
+    string private constant ERROR_OWNER_MOCK_COMMIT_CHECK_REVERTED = "CRV_OWNER_MOCK_COMMIT_CHECK_REVERTED";
+    string private constant ERROR_OWNER_MOCK_REVEAL_CHECK_REVERTED = "CRV_OWNER_MOCK_REVEAL_CHECK_REVERTED";
+    string private constant ERROR_OWNER_MOCK_LEAK_CHECK_REVERTED = "CRV_OWNER_MOCK_LEAK_CHECK_REVERTED";
+
     ICRVoting internal voting;
     bool internal failing;
     mapping (address => uint64) internal weights;
@@ -27,7 +31,15 @@ contract CRVotingOwnerMock is ICRVotingOwner {
 
     function getVoterWeightToCommit(uint256 /* _voteId */, address _voter) external returns (uint64) {
         if (failing) {
-            revert('CRV_OWNER_MOCK_COMMIT_CHECK_REVERTED');
+            revert(ERROR_OWNER_MOCK_COMMIT_CHECK_REVERTED);
+        }
+
+        return weights[_voter];
+    }
+
+    function getVoterWeightToLeak(uint256 /* _voteId */, address _voter) external returns (uint64) {
+        if (failing) {
+            revert(ERROR_OWNER_MOCK_LEAK_CHECK_REVERTED);
         }
 
         return weights[_voter];
@@ -35,7 +47,7 @@ contract CRVotingOwnerMock is ICRVotingOwner {
 
     function getVoterWeightToReveal(uint256 /* _voteId */, address _voter) external returns (uint64) {
         if (failing) {
-            revert('CRV_OWNER_MOCK_REVEAL_CHECK_REVERTED');
+            revert(ERROR_OWNER_MOCK_REVEAL_CHECK_REVERTED);
         }
 
         return weights[_voter];
--- test/voting/crvoting-reveal.js
+++ test/voting/crvoting-reveal.js
@@ -6,6 +6,8 @@ const { assertEvent, assertAmountOfEvents } = require('../helpers/assertEvent')
 const CRVoting = artifacts.require('CRVoting')
 const CRVotingOwner = artifacts.require('CRVotingOwnerMock')
 
+const ERROR_INVALID_OUTCOME = "CRV_INVALID_OUTCOME"
+
 contract('CRVoting reveal', ([_, voter]) => {
   let voting, votingOwner
 
@@ -30,40 +32,8 @@ contract('CRVoting reveal', ([_, voter]) => {
         })
 
         context('when the given voter has not voted before', () => {
-          context('when the owner does not revert when checking the weight of the voter', () => {
-            context('when the owner tells a weight greater than zero', () => {
-              const weight = 10
-
-              beforeEach('mock voter weight', async () => {
-                await votingOwner.mockVoterWeight(voter, weight)
-              })
-
-              it('reverts', async () => {
-                await assertRevert(voting.reveal(voteId, OUTCOMES.LOW, SALT, { from: voter }), 'CRV_INVALID_COMMITMENT_SALT')
-              })
-            })
-
-            context('when the owner tells a zeroed weight', () => {
-              const weight = 0
-
-              beforeEach('mock voter weight', async () => {
-                await votingOwner.mockVoterWeight(voter, weight)
-              })
-
-              it('reverts', async () => {
-                await assertRevert(voting.reveal(voteId, OUTCOMES.LOW, SALT, { from: voter }), 'CRV_REVEAL_DENIED_BY_OWNER')
-              })
-            })
-          })
-
-          context('when the owner reverts when checking the weight of the voter', () => {
-            beforeEach('mock the owner to revert', async () => {
-              await votingOwner.mockChecksFailing(true)
-            })
-
-            it('reverts', async () => {
-              await assertRevert(voting.reveal(voteId, OUTCOMES.LOW, SALT, { from: voter }), 'CRV_OWNER_MOCK_REVEAL_CHECK_REVERTED')
-            })
+          it('reverts', async () => {
+            await assertRevert(voting.reveal(voteId, OUTCOMES.LOW, SALT, { from: voter }), 'CRV_INVALID_COMMITMENT_SALT')
           })
         })
 
@@ -165,7 +135,7 @@ contract('CRVoting reveal', ([_, voter]) => {
                 })
 
                 it('reverts', async () => {
-                  await assertRevert(voting.reveal(voteId, OUTCOMES.LOW, SALT, { from: voter }), 'CRV_REVEAL_DENIED_BY_OWNER')
+                  await assertRevert(voting.reveal(voteId, committedOutcome, SALT, { from: voter }), 'CRV_REVEAL_DENIED_BY_OWNER')
                 })
               })
             })
@@ -262,6 +232,19 @@ contract('CRVoting reveal', ([_, voter]) => {
             })
           }
 
+          const itHandlesInvalidOutcomeRevealedVotesFor = committedOutcome => {
+            const commitment = encryptVote(committedOutcome)
+
+            beforeEach('commit a vote', async () => {
+              await votingOwner.mockVoterWeight(voter, 10)
+              await voting.commit(voteId, commitment, { from: voter })
+            })
+
+            it('reverts', async () => {
+              await assertRevert(voting.reveal(voteId, committedOutcome, SALT, { from: voter }), ERROR_INVALID_OUTCOME)
+            })
+          }
+
           context('when the given voter committed a valid outcome', async () => {
             itHandlesValidRevealedVotesFor(OUTCOMES.LOW)
           })
@@ -271,15 +254,15 @@ contract('CRVoting reveal', ([_, voter]) => {
           })
 
           context('when the given voter committed a missing outcome', async () => {
-            itHandlesInvalidRevealedVotesFor(OUTCOMES.MISSING)
+            itHandlesInvalidOutcomeRevealedVotesFor(OUTCOMES.MISSING)
           })
 
           context('when the given voter committed a leaked outcome', async () => {
-            itHandlesInvalidRevealedVotesFor(OUTCOMES.LEAKED)
+            itHandlesInvalidOutcomeRevealedVotesFor(OUTCOMES.LEAKED)
           })
 
           context('when the given voter committed an out-of-bounds outcome', async () => {
-            itHandlesInvalidRevealedVotesFor(OUTCOMES.HIGH.add(bn(1)))
+            itHandlesInvalidOutcomeRevealedVotesFor(OUTCOMES.HIGH.add(bn(1)))
           })
         })
       })

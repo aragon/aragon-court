d8c767b40bce9edd432d2e25844744639d00b9f9
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2019-11-04 22:38:31 -0300

    contracts: drop `SafeERC20` static invoke

--- contracts/lib/os/SafeERC20.sol
+++ contracts/lib/os/SafeERC20.sol
@@ -14,73 +14,6 @@ library SafeERC20 {
     string private constant ERROR_TOKEN_BALANCE_REVERTED = "SAFE_ERC_20_BALANCE_REVERTED";
     string private constant ERROR_TOKEN_ALLOWANCE_REVERTED = "SAFE_ERC_20_ALLOWANCE_REVERTED";
 
-    function invokeAndCheckSuccess(address _addr, bytes memory _calldata)
-    private
-    returns (bool)
-    {
-        bool ret;
-        assembly {
-            let ptr := mload(0x40)    // free memory pointer
-
-            let success := call(
-            gas,                  // forward all gas
-            _addr,                // address
-            0,                    // no value
-            add(_calldata, 0x20), // calldata start
-            mload(_calldata),     // calldata length
-            ptr,                  // write output over free memory
-            0x20                  // uint256 return
-            )
-
-            if gt(success, 0) {
-            // Check number of bytes returned from last function call
-                switch returndatasize
-
-                // No bytes returned: assume success
-                case 0 {
-                    ret := 1
-                }
-
-                // 32 bytes returned: check if non-zero
-                case 0x20 {
-                // Only return success if returned data was true
-                // Already have output in ptr
-                    ret := eq(mload(ptr), 1)
-                }
-
-                // Not sure what was returned: don't mark as success
-                default { }
-            }
-        }
-        return ret;
-    }
-
-    function staticInvoke(address _addr, bytes memory _calldata)
-    private
-    view
-    returns (bool, uint256)
-    {
-        bool success;
-        uint256 ret;
-        assembly {
-            let ptr := mload(0x40)    // free memory pointer
-
-            success := staticcall(
-            gas,                  // forward all gas
-            _addr,                // address
-            add(_calldata, 0x20), // calldata start
-            mload(_calldata),     // calldata length
-            ptr,                  // write output over free memory
-            0x20                  // uint256 return
-            )
-
-            if gt(success, 0) {
-                ret := mload(ptr)
-            }
-        }
-        return (success, ret);
-    }
-
     /**
     * @dev Same as a standards-compliant ERC20.transfer() that never reverts (returns false).
     *      Note that this makes an external call to the token.
@@ -121,36 +54,41 @@ library SafeERC20 {
         return invokeAndCheckSuccess(address(_token), approveCallData);
     }
 
-    /**
-    * @dev Static call into ERC20.balanceOf().
-    * Reverts if the call fails for some reason (should never fail).
-    */
-    function staticBalanceOf(ERC20 _token, address _owner) internal view returns (uint256) {
-        bytes memory balanceOfCallData = abi.encodeWithSelector(
-            _token.balanceOf.selector,
-            _owner
-        );
+    function invokeAndCheckSuccess(address _addr, bytes memory _calldata) private returns (bool) {
+        bool ret;
+        assembly {
+            let ptr := mload(0x40)    // free memory pointer
 
-        (bool success, uint256 tokenBalance) = staticInvoke(address(_token), balanceOfCallData);
-        require(success, ERROR_TOKEN_BALANCE_REVERTED);
+            let success := call(
+                gas,                  // forward all gas
+                _addr,                // address
+                0,                    // no value
+                add(_calldata, 0x20), // calldata start
+                mload(_calldata),     // calldata length
+                ptr,                  // write output over free memory
+                0x20                  // uint256 return
+            )
 
-        return tokenBalance;
-    }
+            if gt(success, 0) {
+            // Check number of bytes returned from last function call
+                switch returndatasize
 
-    /**
-    * @dev Static call into ERC20.allowance().
-    * Reverts if the call fails for some reason (should never fail).
-    */
-    function staticAllowance(ERC20 _token, address _owner, address _spender) internal view returns (uint256) {
-        bytes memory allowanceCallData = abi.encodeWithSelector(
-            _token.allowance.selector,
-            _owner,
-            _spender
-        );
+                // No bytes returned: assume success
+                case 0 {
+                    ret := 1
+                }
 
-        (bool success, uint256 allowance) = staticInvoke(address(_token), allowanceCallData);
-        require(success, ERROR_TOKEN_ALLOWANCE_REVERTED);
+                // 32 bytes returned: check if non-zero
+                case 0x20 {
+                // Only return success if returned data was true
+                // Already have output in ptr
+                    ret := eq(mload(ptr), 1)
+                }
 
-        return allowance;
+                // Not sure what was returned: don't mark as success
+                default { }
+            }
+        }
+        return ret;
     }
 }

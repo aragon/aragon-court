9fc3944c8402ff31f8fda51699eca0ea7d13e672
ßingen <bingen@aragon.one>
2020-02-17 18:49:19 +0100

    Add juror treasury balances (#34)
    * Add juror treasury balances
    
    * Fix treasury token balances after subscriptions token change
    
    Also, change name of JurorTreasuryToken.
    Address PR #34 comments.
    
    * Remove juror-token update from subscription fees claim
    
    It's not needed because tokens go directly to juror's wallet.
    Address PR #34 comments.

--- src/Treasury.ts
+++ src/Treasury.ts
@@ -1,7 +1,8 @@
 import { buildId } from '../helpers/id'
-import { FeeMovement } from '../types/schema'
-import { FeesClaimed } from '../types/templates/Subscriptions/Subscriptions'
-import { Assign, Withdraw } from '../types/templates/Treasury/Treasury'
+import { FeeMovement, JurorTreasuryTokenBalance } from '../types/schema'
+import { FeesClaimed, Subscriptions } from '../types/templates/Subscriptions/Subscriptions'
+import { Assign, Withdraw, Treasury } from '../types/templates/Treasury/Treasury'
+import { crypto, Address, ByteArray, EthereumEvent } from '@graphprotocol/graph-ts'
 
 let ASSIGN = 'Assign'
 let WITHDRAW = 'Withdraw'
@@ -15,6 +16,7 @@ export function handleAssign(event: Assign): void {
   movement.amount = event.params.amount
   movement.createdAt = event.block.timestamp
   movement.save()
+  updateJurorToken(event.params.token, event.params.to, event)
 }
 
 export function handleWithdraw(event: Withdraw): void {
@@ -25,6 +27,7 @@ export function handleWithdraw(event: Withdraw): void {
   movement.amount = event.params.amount
   movement.createdAt = event.block.timestamp
   movement.save()
+  updateJurorToken(event.params.token, event.params.to, event)
 }
 
 export function handleSubscriptionPaid(event: FeesClaimed): void {
@@ -35,4 +38,40 @@ export function handleSubscriptionPaid(event: FeesClaimed): void {
   movement.amount = event.params.jurorShare
   movement.createdAt = event.block.timestamp
   movement.save()
+}
+
+function updateJurorToken(token: Address, juror: Address, event: EthereumEvent): void {
+  let jurorToken = loadOrCreateJurorToken(token, juror)
+  let treasury = Treasury.bind(event.address)
+  let balance = treasury.balanceOf(token, juror)
+  jurorToken.balance = balance
+  jurorToken.save()
+}
+
+function loadOrCreateJurorToken(token: Address, juror: Address): JurorTreasuryTokenBalance | null {
+  let id = buildJurorTokenId(token, juror)
+  let jurorToken = JurorTreasuryTokenBalance.load(id)
+
+  if (jurorToken === null) {
+    jurorToken = new JurorTreasuryTokenBalance(id)
+    jurorToken.token = token.toHex()
+    jurorToken.juror = juror.toHex()
+  }
+
+  return jurorToken
+}
+
+function buildJurorTokenId(token: Address, juror: Address): string {
+  return crypto.keccak256(concat(token, juror)).toHex()
+}
+
+function concat(a: ByteArray, b: ByteArray): ByteArray {
+  let out = new Uint8Array(a.length + b.length)
+  for (let i = 0; i < a.length; i++) {
+    out[i] = a[i]
+  }
+  for (let j = 0; j < b.length; j++) {
+    out[a.length + j] = b[j]
+  }
+  return out as ByteArray
 }

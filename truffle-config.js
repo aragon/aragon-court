a1d995190e4d1680732e14d97d9df635ef672512
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2020-08-12 14:02:36 -0300

    Chore: Add coverage truffle plugin

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,6 @@
 const TruffleConfig = require('@aragon/truffle-config-v5/truffle-config')
 
+TruffleConfig.plugins = ["solidity-coverage"]
 TruffleConfig.compilers.solc.version = '0.5.8'
 TruffleConfig.compilers.solc.settings.optimizer.runs = 3000 // DisputesManager module is hitting size limit with 10k runs
 

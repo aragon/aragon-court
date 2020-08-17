b1f8286e4ad345ab0b24c98ab2b14d8c9a0dff5b
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2020-08-17 16:02:47 -0300

    Chore: Add solidity coverage v7 (#280)
    * Chore: Add coverage GH job
    
    * Chore: Add coverage truffle plugin
    
    * chore: remove GH action for coverage

--- .solcover.js
+++ .solcover.js
@@ -1,12 +1,9 @@
-const skipFiles = [
-  'lib',
-  'test',
-  'standards'
-]
-
 module.exports = {
-    norpc: true,
-    compileCommand: '../node_modules/.bin/truffle compile',
-    testCommand: 'node --max-old-space-size=4096 ../node_modules/.bin/truffle test --network coverage',
-    skipFiles,
+  norpc: true,
+  copyPackages: ['@aragon/contract-helpers-test'],
+  skipFiles: [
+    'lib',
+    'test',
+    'standards'
+  ],
 }

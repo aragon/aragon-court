6ec7324237bf64b5c2cc1202b15ac7a4e8638dc1
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2020-08-28 09:23:53 -0300

    Disputable: Support agreement app ID for the precedence campaign (#76)
    * Disputable: Support agreement app id for precedence campaign
    
    * disputable: clarify app IDs

--- helpers/disputable.ts
+++ helpers/disputable.ts
@@ -3,18 +3,23 @@ import { Agreement } from '../types/templates/DisputeManager/Agreement'
 import { Disputable, Dispute } from '../types/schema'
 import { crypto, Bytes, Address, BigInt } from '@graphprotocol/graph-ts'
 
-// appId for 'agreement.open.aragonpm.eth'
-const AGREEMENT_DISPUTE_HEADER = '34c62f3aec3073826f39c2c35e9a1297d9dbf3cc77472283106f09eee9cf47bf'
+// App ID for disputable-voting.precedence-campaign.aragonpm.eth:
+const AGREEMENT_OPEN_APP_ID = '34c62f3aec3073826f39c2c35e9a1297d9dbf3cc77472283106f09eee9cf47bf'
+// App ID for agreement.precedence-campaign.aragonpm.eth:
+const AGREEMENT_PC_APP_ID = '15a969a0e134d745b604fb43f699bb5c146424792084c198d53050c4d08126d1'
+
+const AGREEMENT_APP_ID_LENGTH = AGREEMENT_PC_APP_ID.length
 const AGREEMENT_DISPUTE_METADATA_LENGTH = 64 // "[APP_ID][CHALLENGE_ID]" = 32 + 32
 
 export function tryDecodingAgreementMetadata(dispute: Dispute): void {
   let rawMetadata = dispute.rawMetadata
   if (rawMetadata.length != AGREEMENT_DISPUTE_METADATA_LENGTH) return
 
-  let header = rawMetadata.subarray(0, AGREEMENT_DISPUTE_HEADER.length / 2) as Bytes
-  if (header.toHexString().slice(2) != AGREEMENT_DISPUTE_HEADER) return
+  let header = rawMetadata.subarray(0, AGREEMENT_APP_ID_LENGTH / 2) as Bytes
+  let actualAppId = header.toHexString().slice(2)
+  if (actualAppId != AGREEMENT_OPEN_APP_ID && actualAppId != AGREEMENT_PC_APP_ID) return
 
-  let rawChallengeId = rawMetadata.subarray(AGREEMENT_DISPUTE_HEADER.length / 2, rawMetadata.length) as Bytes
+  let rawChallengeId = rawMetadata.subarray(AGREEMENT_APP_ID_LENGTH / 2, rawMetadata.length) as Bytes
   let challengeId = BigInt.fromSignedBytes(rawChallengeId.reverse() as Bytes)
   let agreement = Agreement.bind(Address.fromString(dispute.subject))
   let challengeData = agreement.try_getChallenge(challengeId)

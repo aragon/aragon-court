2d9293d4552e0bd56929b779ea206033e54f1c72
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2020-09-08 07:44:27 -0300

    Chore: Automate deployments (#80)
    * chore: automate deployments
    
    * chore: add install deps step to deployment action
    
    * chore: fix GRAPHKEY secret name
    
    * chore: fix deployment step
    
    * chore: fetch subgraph ID in deployment script
    
    * chore: fix GRAPHKEY secret name for testnets
    
    * chore: automate aragon infra deployment
    
    * chore: fix aragon infra deployment
    
    * chore: fix aragon infra deployment
    
    * chore: deploy to testnets only on master
    
    * chore: reassign subgraphs after deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,12 @@ set -o errexit
 # Run graph build
 npm run build:graph
 
+# Require $GRAPHKEY to be set
+if [[ -z "${GRAPHKEY}" ]]; then
+  echo "Please set \$GRAPHKEY to your The Graph access token to run this command."
+  exit 1
+fi
+
 # Use custom subgraph name based on target network
 if [[ "$NETWORK" != "mainnet" ]]; then
   SUBGRAPH_EXT="-${NETWORK}"
@@ -30,4 +36,20 @@ fi
 }
 
 # Deploy subgraph
-graph deploy aragon/aragon-court${SUBGRAPH_EXT} --ipfs ${IPFS_NODE} --node ${GRAPH_NODE}
+graph deploy aragon/aragon-court${SUBGRAPH_EXT} \
+  --ipfs ${IPFS_NODE} \
+  --node ${GRAPH_NODE} \
+  --access-token "$GRAPHKEY" > deploy-output.txt
+
+SUBGRAPH_ID=$(grep "Build completed:" deploy-output.txt | grep -oE "Qm[a-zA-Z0-9]{44}")
+rm deploy-output.txt
+echo "The Graph deployment complete: ${SUBGRAPH_ID}"
+
+if [[ -z "$SUBGRAPH_ID" ]]; then
+  echo "Could not find subgraph ID in deploy output, cannot deploy to Aragon infra."
+else
+  echo "Deploying subgraph ${SUBGRAPH_ID} to Aragon infra..."
+  kubectl exec graph-shell -- create aragon/aragon-court${SUBGRAPH_EXT}
+  kubectl exec graph-shell -- deploy aragon/aragon-court${SUBGRAPH_EXT} ${SUBGRAPH_ID} graph_index_node_0
+  kubectl exec graph-shell -- reassign aragon/aragon-court${SUBGRAPH_EXT} ${SUBGRAPH_ID} graph_index_node_0
+fi

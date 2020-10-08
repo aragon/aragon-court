46a00c1a682f854b8e28c1276cd7f862e2c9b42b
William Griffiths <willjgriff@gmail.com>
2020-10-08 14:27:06 +0100

    Update BrightIdRegister RegisterAndCall interface.

--- contracts/brightid/RegisterAndCall.sol
+++ contracts/brightid/RegisterAndCall.sol
@@ -5,8 +5,10 @@ contract RegisterAndCall {
     /**
     * @dev This allows users to verify their BrightId account and interact with a contract in one transaction.
     *      Implementers of this function should check that msg.sender is the BrightIdRegister contract expected.
+    * @param _usersSenderAddress The address from which the transaction was created
     * @param _usersUniqueId The unique address assigned to the registered BrightId user
     * @param _data Optional data that can be used to determine what operations to execute in the recipient contract
     */
-    function receiveRegistration(address _usersUniqueId, bytes calldata _data) external;
+    function receiveRegistration(address _usersSenderAddress, address _usersUniqueId, bytes calldata _data) external;
+
 }

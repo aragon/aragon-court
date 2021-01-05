a46fbc99052da7d6daeee4a8563cf14df4743807
William Griffiths <willjgriff@gmail.com>
2021-01-05 16:51:19 +0000

    Fix controller tests.

--- test/court/controller/controller-config.js
+++ test/court/controller/controller-config.js
@@ -15,6 +15,7 @@ contract('Controller', ([_, configGovernor, feesUpdater, someone, drafter, appea
   const jurorFee = bigExp(10, 18)
   const draftFee = bigExp(30, 18)
   const settleFee = bigExp(40, 18)
+  const maxRulingOptions = bn(2)
   const evidenceTerms = bn(1)
   const commitTerms = bn(1)
   const revealTerms = bn(2)
@@ -41,6 +42,7 @@ contract('Controller', ([_, configGovernor, feesUpdater, someone, drafter, appea
       jurorFee,
       draftFee,
       settleFee,
+      maxRulingOptions,
       evidenceTerms,
       commitTerms,
       revealTerms,
